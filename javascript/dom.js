7295991b121b8265819e92a66d6a01b540223b3a
Kaif Mohamad Shaik <mohamadkaifshaik20@gmail.com>
2026-02-21 10:03:21 +0530

    DOM

---
+++ javascript/dom.js
@@ -0,0 +1,23 @@
+// //DOM
+
+// let wfh=document.getElementById("emp");
+// wfh.innerHTML+="<p>Work From Home: 2</p>"
+
+// let new_menu=document.getElementsByClassName("menu_1")
+// // new_menu[0].style.color="red";
+// for(let i of new_menu)
+//   i.style.color="darkgreen"
+
+// // let new_anc=document.getElementsByTagName("a");
+// // for(let i of new_anc)
+// //   i.style.color="red";
+
+// // let abc=document.querySelector(".mer");
+// // abc.innerText="welecome";
+
+// // let ab=document.querySelector(".a_tag");
+// // ab.style.color="yellow";
+
+// let ab_c=document.querySelectorAll(".a_tag");
+// for(let i of ab_c)
+//   i.style.color="red"
