75a4ce964e743115e4bee1ac8818c169d796f01f
Kaif Mohamad Shaik <mohamadkaifshaik20@gmail.com>
2026-03-27 07:37:52 +0530

    ASP Prac

---
+++ dotnet/asp/ContactManage/ContactManage/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using ContactManage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ContactManage.Controllers
+{
+    public class ContactController : Controller
+    {
+        static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        public ActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+
+        public ActionResult GetContactById(int id)
+        {
+            var res = contacts.FirstOrDefault(x => x.ContactId == id);
+            return View(res);
+        }
+
+        public ActionResult AddContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContact(ContactInfo contactInfo)
+        {
+            contacts.Add(contactInfo);
+            return RedirectToAction("ShowContacts");
+        }
+    }
+}
